c26e7726820d86b34ece5395e53d2edc8ca5a5f4
ZNBrown <ZachNatBrown@gmail.com>
2021-11-10 14:16:41 +0000

    stashing

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import './style.css'
 
-export const Header = () => <nav>Navbar soon</nav>;
+export const Header = () =>  {
+    return (
+        <div className={`header`} >
+        {/* <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt="Not our logo" /> */}
+        <h1>Welcome</h1>
+    </div>
+    )
+};
