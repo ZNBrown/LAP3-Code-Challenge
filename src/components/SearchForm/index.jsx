2685c2501f6ca0d8fe2c070aec2b740d2f826096
ZNBrown <ZachNatBrown@gmail.com>
2021-11-10 10:32:27 +0000

    corrects search form

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,70 +1,42 @@
+import { co } from 'co';
+import { cos } from 'prelude-ls';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './style.css';
 
-// export const SearchForm = () => {
-//     const dispatch = useDispatch();
-
-//     const [ dateInput, setDateInput ] = useState("");
-//     const [ formAlert, setFormAlert ] = useState("Select a date");
-
-//     const reset = () => {
-//         setDateInput("");
-//         setFormAlert("Select a date")
-//     }
-
-//     const handleSubmit = e => {
-//         e.preventDefault()
-//         dispatch(updateDate(dateInput))
-//         reset();
-//     }
-
-//     const handleInput = e => {
-//         const selectedDate = new Date(e.target.value)
-//         const today = new Date()
-//         if (selectedDate <= today) {            
-//             setDateInput(formatDate(selectedDate))
-//             setFormAlert("")
-//         } else {
-//             setFormAlert("Date cannot be in the future")
-//         }
-//     }
-
-//     return (
-//         <form onSubmit={handleSubmit} aria-label="form">
-//             <label htmlFor="date">Date</label>
-//             <input type="date" id="date" value={dateInput} onChange={handleInput}/>
-//             <input
-//                 type="submit"
-//                 disabled={formAlert}
-//                 value={formAlert ? formAlert : "Search"}
-//             />
-//         </form>
-//     );
-// };
 export const SearchForm = () => {
-    const [ formData, setFormData ] = useState({ username: ""})
+    // const dispatch = useDispatch();
+    //     dispatch(updateDate(dateInput))
+
+    const [ formData, setFormData ] = useState("")
+    const [ formAlert, setFormAlert ] = useState("Type a name");
     
     const handleInput = e => {
-        // using destructuring to access e.target.name and e.target.value
-        const { name, value } = e.target;
-        console.log(name, value)
-        // note the use of the [square brackets] here so we can use the name variable (instead of accessing a key of .name)
-        setFormData({ ...formData, [name]: value }); 
+        if (e.target.value.length > 1) {            
+            setFormData(e.target.value)
+            setFormAlert("")
+        } else {
+            setFormAlert("Username must be at least 1 character")
+        }
     };
 
     const handleSubmit = e => {
         e.preventDefault()
-        console.log(e)
+        console.log(e.target[0].value)
         console.log('submitted')
     }
 
     return (
-
-    <form onSubmit={handleSubmit}>
-        <input 
-            type="text" name="nameInput" 
+    <form onSubmit={handleSubmit} aria-label>
+        <label htmlFor="text">Name</label>
+        <input type="text" id="name"  onChange={handleInput} /> 
+        <input
+            type="submit"
+            disabled={formAlert}
+            value={formAlert ? formAlert : "Search"}
         />
     </form>
     );
-}
+
+
+};
