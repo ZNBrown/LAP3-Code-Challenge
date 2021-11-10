7f7147edc337e94542036d9758285ab7dbe97dce
ZNBrown <ZachNatBrown@gmail.com>
2021-11-10 11:50:41 +0000

    begins styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 //this is bodged because we have to export default in navbar for the test to work
 import * as Pages from './pages';
+import { Header, Footer } from './layout';
+
 
 
 
@@ -8,9 +10,11 @@ import * as Pages from './pages';
 function App() {
     return (
         <div id="app">
+            <Header/>
             <main>
                 <Pages.Search/>
             </main>
+            <Footer/>
         </div>
     )
 }
