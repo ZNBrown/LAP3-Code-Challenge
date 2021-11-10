42536bf5c88d0b6c7e37792d7246c394862f430c
olu-akano <akanopaul@yahoo.co.uk>
2021-11-10 16:23:29 +0000

    add link to repo name

--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -4,7 +4,7 @@ export const Card = ({ repoName, url, forks, openIssues, watchers, language}) =>
     return (
         <>
             <div className = "cardCont">
-                <h3>{repoName}</h3>
+                <h3><a href ={link}>{repoName}</a></h3>
                 <p>Forks: {forks}</p>
                 <p>Language: {language}</p>
                 <p>Issues: {openIssues}</p>
